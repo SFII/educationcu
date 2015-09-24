dffd37e55fdb62c94c0bd71400de7b88a9bc15c6
Justin <justinwayneolson@gmail.com>
2015-09-24 13:19:44 -0500

    Added responses

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -9,14 +9,12 @@ var Survey = React.createClass({
    componentDidMount: function(){
       console.log("hello");
       var questions=[{question:"What is your gender"},
-      {options:["Male","Female"]}]
-      console.log(questions)
+      {options:["Male","Female","Perfer not to respond"]}]
       this.setState({myQuestions: questions }, function() {});
    },
    render: function(){
       var myQuestions = this.state.myQuestions.map(function(item){
-         console.log(item.question);
-         return (<QuestionComponent question1={item.question}/>);
+         return (<QuestionComponent question1={item.question} options={item.options}/>);
       })
       return  (
             <div>
@@ -31,9 +29,16 @@ var Survey = React.createClass({
 
 var QuestionComponent = React.createClass({
    render: function(){
+      console.log(this.props.options);
+      var inputs=[];
+         inputs.push(<p>{this.props.question1}</p>);
+      for(var i=0; i<3; i++){
+         inputs.push(<input type="radio" name="sex">{this.props.options[i]}</input>);
+
+      }
       return (
             <div>
-               <p>{this.props.question1}</p>
+               {inputs}
             </div>
    )}
 })
