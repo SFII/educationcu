4f87860d58dd9507915ef69c3c6529d74e5814c1
Conner Simmering <connersimmering@gmail.com>
2015-10-01 13:00:39 -0600

    spotify and result modification

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,47 +1,96 @@
-var React = window.React = require('react'),
-    Timer = require("./timer/Timer"),
-    mountNode = document.getElementById("main");
+var React = window.React = require('react');
+//     Timer = require("./timer/Timer"),
+var mountNode = document.getElementById("main");
+//
+// var TodoList = React.createClass({
+//   render: function() {
+//     var createItem = function(itemText) {
+//       return <li>{itemText}</li>;
+//     };
+//     return <ul>{this.props.items.map(createItem)}</ul>;
+//   }
+// });
+// var TodoApp = React.createClass({
+//   getInitialState: function() {
+//     return {items: [], text: ''};
+//   },
+//   onChange: function(e) {
+//     this.setState({text: e.target.value});
+//   },
+//   handleSubmit: function(e) {
+//     e.preventDefault();
+//     var nextItems = this.state.items.concat([this.state.text]);
+//     var nextText = '';
+//     this.setState({items: nextItems, text: nextText});
+//   },
+//   render: function() {
+//     return (
+//       <div>
+//         <h3>TODO</h3>
+//         <TodoList items={this.state.items} />
+//         <form onSubmit={this.handleSubmit}>
+//           <input onChange={this.onChange} value={this.state.text} />
+//           <button>{'Add #' + (this.state.items.length + 1)}</button>
+//         </form>
+//         <Timer />
+//       </div>
+//     );
+//   }
+// });
+//
+//
+// React.render(<TodoApp />, mountNode);
 
-var Survey = React.createClass({
-   getInitialState: function(){
-      return {myQuestions:[]};
-   },
-   componentDidMount: function(){
-      console.log("hello");
-      var questions=[{question:"What is your gender"},
-      {options:["Male","Female","Perfer not to respond"]}]
-      this.setState({myQuestions: questions }, function() {});
-   },
-   render: function(){
-      var myQuestions = this.state.myQuestions.map(function(item){
-         return (<QuestionComponent question1={item.question} options={item.options}/>);
-      })
-      return  (
-            <div>
-               <p>
-                  {myQuestions}
-               </p>
-            </div>
-            )
+var MyComp = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    // logic, go pull data from database.
+    //$.get("http://myapi.com/")
 
-       }
-   })
+    // var x = [{name: "Peyman", age: "99"},
+    //             {name: "Slaten", age: "69"}]
 
-var QuestionComponent = React.createClass({
-   render: function(){
-      console.log(this.props.options);
-      var inputs=[];
-         inputs.push(<p>{this.props.question1}</p>);
-      for(var i=0; i<3; i++){
-         inputs.push(<input type="radio" name="sex">{this.props.options[i]}</input>);
+    //var x = $.get()
+    $.get("https://api.spotify.com/v1/albums?ids=382ObEPsp2rxGrnsizN5TX,1A2GTWGtFfWp7KSQTwWOyo,2noRn2Aes5aoNVsU6iWThc&market=ES", function(result) {
+	    result = {"survey":[
+			{"question":"What is your sex?", "responses":["M","F"]},
+			]};
+      this.setState({data: result.survey}, null );
+    }.bind(this));
 
-      }
-      return (
-            <div>
-               {inputs}
-            </div>
-   )}
+
+  },
+  render: function () {
+
+    var myDataShit = this.state.data.map(function(item){
+      return (<SubComp prop1={item.question} prop2={item.responses} />);
+    })
+
+    console.log(myDataShit);
+
+    return (
+      <div>
+        Yo, my data:
+        <p id='hello'>
+          {myDataShit}
+        </p>
+      </div>
+    )
+  }
+})
+
+var SubComp = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <b>{this.props.prop1}</b>
+        <b>{this.props.prop2}</b>
+      </div>
+    )
+  }
 })
 
 
-React.render(<Survey />, mountNode);
+React.render(<MyComp />, mountNode);
