b17f79ba12723e5dfc47923c583c90ae2f042a60
Justin <justinwayneolson@gmail.com>
2015-10-05 17:31:18 -0500

    added dynamic radio button names

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -2,61 +2,90 @@ var React = window.React = require('react');
 var mountNode = document.getElementById("main");
 
 var SubComp = React.createClass({
-  render: function () {
-    return (
-            <div>
+    render: function() {
+        return ( <div>
               <br></br>
-              <p>{this.props.question}</p>
-              {this.props.responses.map(function(response) {
-                 return (<div>
-                            <input type="radio" name="sex">{response}</input>
-                            <br></br>
-                        </div>
-                        );
-               })}
-            </div>
+              <p> {
+                this.props.question
+            } </p> {
+                this.props.responses.map(function(response) {
+                   console.log(name);
+                    return ( <div>
+                        <input type = "radio"
+                        name = {name}> {
+                            response
+                        } </input> <br> </br> </div>
+                    );
+                })
+            } </div>
         )
 
-     }
-  })
-
-var MyComp = React.createClass({
-  getInitialState: function() {
-    return {data: []};
-  },
-  componentDidMount: function() {
-    $.get("https://api.spotify.com/v1/albums?ids=382ObEPsp2rxGrnsizN5TX,1A2GTWGtFfWp7KSQTwWOyo,2noRn2Aes5aoNVsU6iWThc&market=ES", function(result) {
-	    result = {"survey":[
-			{"question":"What is your sex?", "responses":["M","F"],"id":1},
-			{"question":"What is your current status?", "responses":["Freshaman","Sophmore","Junior","Senior"],"id":1},
-         {"question":"Would you reccommend the course to a friend", "responses":["Yes","No"],"id":1},
-         {"question":"Would you reccommend the professor to a friend", "responses":["Yes","No"],"id":1}
-			]};
-      this.setState({data: result.survey}, null );
-    }.bind(this));
-
-
-  },
-  render: function () {
-
-    var myDataShit = this.state.data.map(function(item){
-       console.log(item);
-      return (<SubComp id={item.id} question={item.question} responses={item.responses}/>);
-    })
-
-
-    return (
-      <div>
-         <form>
-           <p id='hello'>
-             {myDataShit}
-           </p>
-           </form>
-      </div>
-    )
-  }
+    }
 })
 
+var MyComp = React.createClass({
+            getInitialState: function() {
+                return {
+                    data: []
+                };
+            },
+            componentDidMount: function() {
+                $.get("https://api.spotify.com/v1/albums?ids=382ObEPsp2rxGrnsizN5TX,1A2GTWGtFfWp7KSQTwWOyo,2noRn2Aes5aoNVsU6iWThc&market=ES", function(result) {
+                    result = {
+                        "survey": [{
+                            "question": "What is your sex?",
+                            "responses": ["M", "F"],
+                            "name": "one"
+                        }, {
+                            "question": "What is your current status?",
+                            "responses": ["Freshaman", "Sophmore", "Junior", "Senior"],
+                            "name": "two"
+                        }, {
+                            "question": "Would you reccommend the course to a friend",
+                            "responses": ["Yes", "No"],
+                            "name": "three"
+                        }, {
+                            "question": "Would you reccommend the professor to a friend",
+                            "responses": ["Yes", "No"],
+                            "name": "four"
+                        }, {
+                           "question": "Did you enjoy this course?",
+                           "responses": ["Yes", "No"],
+                           "name": "five"
+                        }]
+                    };
+                    this.setState({
+                        data: result.survey
+                    }, null);
+                }.bind(this));
 
 
-React.render(<MyComp />, mountNode);
+            },
+            render: function() {
+
+                var myData = this.state.data.map(function(item) {
+                        return ( <SubComp name = {
+                                item.name
+                            }
+                            question = {
+                                item.question
+                            }
+                            responses = {
+                                item.responses
+                            }
+                            />);
+                        })
+
+
+                    return ( <div>
+                        <form>
+                        <p id = 'hello'> {
+                            myData
+                        } </p> </form> </div>
+                    )
+                }
+            })
+
+
+
+React.render( <MyComp/> , mountNode);
